6119d96eb97e7340c554542c1f02887f48bbc034
gustadev24 <lgsc21211@gmail.com>
2025-11-13 07:39:51 -0500

    feat: configure path aliases for imports

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,47 +1,56 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import path from "path";
 
 const phasermsg = () => {
-    return {
-        name: 'phasermsg',
-        buildStart() {
-            process.stdout.write(`Building for production...\n`);
-        },
-        buildEnd() {
-            const line = "---------------------------------------------------------";
-            const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
-            process.stdout.write(`${line}\n${msg}\n${line}\n`);
-            
-            process.stdout.write(`✨ Done ✨\n`);
-        }
-    }
-}   
+  return {
+    name: "phasermsg",
+    buildStart() {
+      process.stdout.write(`Building for production...\n`);
+    },
+    buildEnd() {
+      const line = "---------------------------------------------------------";
+      const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
+      process.stdout.write(`${line}\n${msg}\n${line}\n`);
+
+      process.stdout.write(`✨ Done ✨\n`);
+    },
+  };
+};
 
 export default defineConfig({
-    base: './',
-    logLevel: 'warning',
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
+  base: "./",
+  logLevel: "warning",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../src"),
+      "@/scenes": path.resolve(__dirname, "../src/scenes"),
+      "@/entities": path.resolve(__dirname, "../src/entities"),
+      "@/core": path.resolve(__dirname, "../src/core"),
+      "@/ui": path.resolve(__dirname, "../src/ui"),
+      "@/assets": path.resolve(__dirname, "../src/assets"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ["phaser"],
         },
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                passes: 2
-            },
-            mangle: true,
-            format: {
-                comments: false
-            }
-        }
+      },
     },
-    server: {
-        port: 8080
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        passes: 2,
+      },
+      mangle: true,
+      format: {
+        comments: false,
+      },
     },
-    plugins: [
-        phasermsg()
-    ]
+  },
+  server: {
+    port: 8080,
+  },
+  plugins: [phasermsg()],
 });
