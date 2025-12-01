e3049b181a0b5da4ddd627946491ee99b97abbf4
gustadev24 <lgsc21211@gmail.com>
2025-11-30 21:49:05 -0500

    feat: click on other nodes to asign energy in and edge

--- src/application/constants/game-constants.ts
+++ src/application/constants/game-constants.ts
@@ -3,5 +3,5 @@ export const GAME_CONSTANTS = {
   DOMINANCE_DURATION_MS: 10_000,
   TIME_LIMIT_MS: 180_000, // 3 minutes
   DEFAULT_SPEED: 0.002, // Velocidad ajustada para distancias normalizadas
-  // DOMINANCE_THRESHOLD = 0.7; // 70%
+  ASSIGNMENT_AMOUNT: 10,
 } as const;
